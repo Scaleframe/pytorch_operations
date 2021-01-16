c767561599d5bf6fc69af3c0c2efd967169c61e8
Jim Devine <jiim.devine@gmail.com>
2021-01-15 20:16:03 -0500

    adds tensor conversion, math and comparision operations

--- 01-pytorch-tensors.py
+++ 01-pytorch-tensors.py
@@ -37,9 +37,52 @@ x = torch.diag(torch.ones(3)) # creates these values at diagonal.
 # how to init and convert tensors to other types, int, float, double
 tensor = torch.arange(4)
 print(tensor.bool()) # boolean True/False
-print(tensor.short()) # int16
+print(tensor.short()) # intzz16
 print(tensor.long()) # int64 (Important)
 print(tensor.half()) # float16
 print(tensor.float()) # float32 (Important)
 print(tensor.double()) # float64
 
+
+# array to tensor conversions
+import numpy as np
+np_array = np.zeros((5,5))
+tensor = torch.from_numpy(np_array)
+np_array_back = tensor.numpy()
+
+
+
+# tensor math operations
+
+x = torch.tensor([1,2,3])
+y = torch.tensor([9,8,7])
+
+z1 = torch.empty(3)
+
+# additon operation, all 3 identical
+torch.add(x, y, out=z1)
+z2 = torch.add(x, y)
+z = x + y
+
+# subtraction
+z = x - y
+
+# division:
+z = torch.true_divide(x, y) 
+# if same shape, will divide element wise
+# if not, will broadcast scalar across tensor
+
+
+# inplace operation
+t = torch.zeros(3)
+t.add_(x) # can be more computationally efficient. 
+t += x # in place
+t = t + x # creates a copy
+
+# exponents:
+z = x.pow(2)
+x = z ** 2 # same operation
+
+# comparison
+z = x > 0
+z = x < 8
